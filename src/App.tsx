4d5ede178d20130b302c32f6c6dc6639d457a772
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-10-18 13:15:04 +0300

    Add unmock.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
 import React, {useState, useEffect} from 'react';
 import {Button, StyleSheet, View, Text} from 'react-native';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+import unmock from 'unmock-react-native';
+
+const useUnmock = true;
 
 const CAT_FACT_URL =
   'https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=1';
 
 const fetchFact = async () => {
   console.log(`Fetching new cat fact`);
-  const fetchResult = await fetch(CAT_FACT_URL);
-  if (!fetchResult.ok) {
-    throw Error(`Failed fetching cat fact with code: ${fetchResult.status}`);
+  if (useUnmock) {
+    unmock.on();
+    console.log(`Unmock on`);
   }
-  const body = await fetchResult.json();
-  const fact = body.text;
-  console.log(`Got a new fact: ${fact}`);
-  return fact;
+  try {
+    const fetchResult = await fetch(CAT_FACT_URL);
+    if (!fetchResult.ok) {
+      throw Error(`Failed fetching cat fact with code: ${fetchResult.status}`);
+    }
+    const body = await fetchResult.json();
+    const fact = body.text;
+    console.log(`Got a new fact: ${fact}`);
+    return fact;
+  } finally {
+    unmock.off();
+  }
 };
 
 const App = () => {
